eb5fc990e8d5969529bb562e6e4acef6a496d9fa
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2017-12-01 11:11:45 -0500

    HTTP setState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './css/App.css';
 import Clock from './components/Clock';
 import logo from './images/logo.svg';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openForBusiness: null,
+      operationHours: {
+        open_at: null,
+        closed_at: null
+      },
+      time: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://app.akira.md/api/system_status')
+      .then(res => {
+        const systemStatus = res.data;
+        
+        this.setState({
+          openForBusiness: systemStatus.is_open_for_business,
+          operationHours: {
+            open_at: systemStatus.open_hours_today.open_at,
+            closed_at: systemStatus.open_hours_today.close_at
+          },
+          time: systemStatus.system_time
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
