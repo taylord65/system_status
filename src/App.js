8bd583155bcd2553911751a3128891e88f54a27c
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2017-12-01 11:36:18 -0500

    moment, render status and times

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './css/App.css';
 import Clock from './components/Clock';
 import logo from './images/logo.svg';
+import moment from 'moment'
+
 
 class App extends Component {
 
@@ -17,6 +19,10 @@ class App extends Component {
       },
       time: null
     };
+  }
+
+  formatTimeToHourOfDay(time){
+    return moment(time).format("hh:mm a");
   }
 
   componentDidMount() {
@@ -36,12 +42,17 @@ class App extends Component {
   }
 
   render() {
+    const akiraStatus = 'We are ' + (this.state.openForBusiness ? 'open today!' : 'closed today.');
     return (
       <div className="App">
 
         <div className="App-header">
           <img src={logo} alt="Akira Logo"/>
+          <h1>{akiraStatus}</h1>
         </div>
+
+        <h2>{this.formatTimeToHourOfDay(this.state.operationHours.open_at)}</h2>
+        <h2>{this.formatTimeToHourOfDay(this.state.operationHours.closed_at)}</h2>
 
         <Clock/>
 
